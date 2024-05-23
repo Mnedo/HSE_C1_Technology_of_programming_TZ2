5045d70f0cdc11e954de9d6db575b122092aa7c7
Mike <75080258+Mnedo@users.noreply.github.com>
2024-05-23 11:39:10 +0300

    Adding tests for functions

--- src/tests/java/FunctionsTest.java
+++ src/tests/java/FunctionsTest.java
@@ -1,1 +1,30 @@
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class NumberProcessorTest {
+    @Test
+    public void testMin() {
+        int[] numbers = {11, 2, 24, 6};
+        assertEquals(2, Main._min(numbers));
+    }
+
+    @Test
+    public void testMax() {
+        int[] numbers = {11, 2, 24, 6};
+
+        assertEquals(24, Main._max(numbers));
+    }
+
+    @Test
+    public void testSum() {
+        int[] numbers = {11, 2, 24, 6}};
+        assertEquals(43, Main._sum(numbers));
+    }
+
+    @Test
+    public void testMult() {
+        int[] numbers = {-3, -11, 1};
+        assertEquals(33, Main._mult(numbers));
+    }
+}
