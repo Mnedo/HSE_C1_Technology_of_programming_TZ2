c277d3d6cd968609034a20bbb074ca653607939e
Mike <75080258+Mnedo@users.noreply.github.com>
2024-05-23 11:42:44 +0300

    Creating test for checking main.java

--- src/tests/java/MainTest.java
+++ src/tests/java/MainTest.java
@@ -1,1 +1,32 @@
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+        String data = "2 4 17 31";
+        Files.write(Paths.get("filik.txt"), data.getBytes());
+
+        ByteArrayOutputStream res = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Main.main(new String[] {"filik.txt"});
+
+        String out = "Считали файл! Искомые значения:\n" +
+                                "Минимальное:\n" +
+                                "2\n" +
+                                "Максимальное:\n" +
+                                "31\n" +
+                                "Сумма:\n" +
+                                "54\n" +
+                                "Произведение:\n" +
+                                "4216\n";
+
+        assertEquals(out, res.toString());
+    }
+}
